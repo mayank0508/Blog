34b3461e9e1eb8ab75e1979b5f2c3d9e776fbcb3
mayank0508 <thakurmayank0508@gmail.com>
2021-08-19 03:32:37 +0530

    About page done

---
+++ pages/about.js
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+export default function About() {
+  return (
+    <div>
+      <Head>
+        <title>About mf</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>About mf</h1>
+      </main>{' '}
+      <footer>Powered by 💩</footer>
+    </div>
+  );
+}
