1e40cf577f053e25b8d15a86bcaffef06dae0f06
mayank0508 <thakurmayank0508@gmail.com>
2021-08-19 04:07:30 +0530

    Linnking the blog

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import Head from 'next/head';
+import Link from 'next/link';
+import { blogPosts } from '../lib/data';
 
 export default function Home() {
   return (
@@ -10,6 +12,21 @@ export default function Home() {
       <main>
         <h1>My Blog 🚀📦</h1>
       </main>{' '}
+      <div>
+        {blogPosts.map(item => (
+          <div key={item.id}>
+            <div>
+              <Link href={`/blog/${item.id}`}>
+                {' '}
+                {/* this make the link on the title of the blog post */}
+                <a>{item.title}</a>
+              </Link>
+            </div>
+            <div>{item.date.toLocaleString()}</div>
+            <div>{item.content}</div>
+          </div>
+        ))}
+      </div>
       <footer>Powered by 💩</footer>
     </div>
   );
