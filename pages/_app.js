23a8476f8f1d0ffcb1f29caa524854f4e1745e4b
mayank0508 <thakurmayank0508@gmail.com>
2021-08-20 13:39:47 +0530

    tailwind done

--- pages/_app.js
+++ pages/_app.js
@@ -3,21 +3,24 @@ import Link from 'next/link';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link href="/">
-              <a>Home</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/about">
-              <a>About</a>
-            </Link>
-          </li>
-        </ul>
-      </nav>
+    <div className="mx-auto my-10 w-9/12">
+      <header>
+        <h1 className="text-purple-900 font-bold text-6xl my-5">My Blog</h1>
+        <nav>
+          <ul className="flex flex-row space-x-4 justify-right my-4">
+            <li>
+              <Link href="/">
+                <a>Home</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/about">
+                <a>About</a>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
       <Component {...pageProps} />
     </div>
   );
